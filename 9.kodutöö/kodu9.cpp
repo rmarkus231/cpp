edec352af609eedd93c0f0f15f8440431077eb4b
Richard Markus Tins <richar03@ut.ee>
2024-04-10 15:25:59 +0300

    hw9

---
+++ 9.kodutöö/kodu9.cpp
@@ -0,0 +1,7 @@
+#include "kodu9.h"
+
+#if !defined(VPL_TEST)
+int main(){
+	return 0;
+}
+#endif
---
+++ 9.kodutöö/kogu9.h
@@ -0,0 +1,54 @@
+#ifndef KODU9_H
+#define KODU9_H
+#include <vector>
+#include <list>
+#include <deque>
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T1, typename T2, typename T3>
+void yhenda(T1& alg1, T2& alg2, T3& tulem){
+	for(auto i = alg1.begin(); i < alg1.end(); i++){
+		tulem.push_back(*i);
+	}
+	for(auto i = alg2.begin(), i < alg2.end(), i++){
+		tulem.push_back(*i);
+	}
+}
+
+template <typename T>
+ostream& tryki(T& alg, ostream& os){
+	for(auto i = alg.begin();i < alg.end(); i++){
+		os << *i << " ";
+	}
+	os << endl;
+	return os;
+}
+
+template <typename T>
+bool operator<(T& x, T& y){
+	sum1 = 0;
+	sum2 = 0;
+	for(auto i = x.begin(); i<x.end(); i++){
+		sum1 += *i;
+	}
+	for(auto i = y.begin(); i<y.end();i++){
+		sum2 += *i;
+	}
+	if(sum1 < sum2){
+		return true;
+	}else{
+		return false;
+	}
+}
+void lisaJaKustuta(vector<string&> v){
+	for(auto i = v.begin(); i<v.end();i++){
+		if(i->find("tt")!=string::npos || i->find("at")!=string::npos){
+			v.erase(i);
+		}else if(i->find("aa")!=string::npos || i->find("uu")!=string::npos || i->find("oo")!=string::npos){
+			v.insert(i,*i);
+		}
+	}
+}
+#endif
