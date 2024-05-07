2e9012301a85a5207e992eea8810fa2386949807
Richard Markus Tins <richar03@ut.ee>
2024-05-07 18:37:38 +0300

    updated

--- 13.kodutöö/kodu13.cpp
+++ 13.kodutöö/kodu13.cpp
@@ -1,6 +1,7 @@
 #include "kodu13.h"
 #include <iostream>
 #include <map>
+#include <sring>
 
 using namespace std;
 
@@ -17,12 +18,28 @@ void täisarvLoop(){
 				//in map
 				mp[val]++;
 			}
+		}catch(const exc& e){
+			string eof_c = "eof"	
+			if(e == eof_c){
+				break;
+			}else{
+				cout << e.msg << endl;
+			};
 		}catch(exception &err){
 			cout << "Vigane sisend stoi" << endl;
-		}
+		};
 	};
+	//end of loop
+	loeMap(mp);
 };
 
-int getInput(){};
+int getInput(){
+	int x;
+	cin >> x;
+	if(cin.fail()){
+		throw exc("eof");
+	};
+	return x;
+};
 
 void loeMap(map<int,size_t>){};
--- 13.kodutöö/kodu13.h
+++ 13.kodutöö/kodu13.h
@@ -1,8 +1,17 @@
 #ifndef KODU13_H
 #define KODU13_H
+#include <exception>
+#include <string>
+using namespace std;
+
+class exc :public exception{
+	public:
+		string msg{};
+		exc(string m) : msg{m} {};
+};
 
 void täisarvLoop();
 int getInput();
-void loeMap(map<int>,size_t):
+void loeMap(map<int,size_t>):
 
 #endif
